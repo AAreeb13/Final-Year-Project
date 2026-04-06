46f218299e3a4f8c0a6f12c9352339de370ac1d4
Mohammad Areeb Ahmad <areebpopos@pop-os.home>
2026-04-06 22:55:30 +0100

    feat: single-agent with designing ability

--- src/settings.py
+++ src/settings.py
@@ -1,16 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict 
 
 class Settings(BaseSettings):
-    openrouterkey: str
+    OPEN_ROUTER_KEY: str
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        if self.openrouterkey is None:
-            raise ValueError("openrouterkey must be set in the .env file")
+        if self.OPEN_ROUTER_KEY is None:
+            raise ValueError("OPEN_ROUTER_KEY must be set in the .env file")
         
     
     
 settings = Settings()
 
 if __name__ == "__main__":
-    assert settings.openrouterkey is not None, "openrouterkey must be set in the .env file"
+    assert settings.OPEN_ROUTER_KEY is not None, "OPEN_ROUTER_KEY must be set in the .env file"
--- src/single_agent_system/main.py
+++ src/single_agent_system/main.py
@@ -1,4 +1,11 @@
 
+import os
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from pydantic import SecretStr
+
 
 def import_settings():
     from src.settings import settings
@@ -7,5 +14,22 @@ def import_settings():
 
 if __name__ == "__main__":
     settings = import_settings()
-    assert settings.openrouterkey is not None, "openrouterkey must be set in the .env file"
+
+    assert settings.OPEN_ROUTER_KEY is not None, "openrouterkey must be set in the .env file"
     print("Settings imported successfully. openrouterkey is set.")
+    model = ChatOpenAI(
+        api_key=SecretStr(settings.OPEN_ROUTER_KEY),
+        base_url="https://openrouter.ai/api/v1",
+        model="gpt-4o-mini",
+        temperature=0.7,
+        max_completion_tokens=1000)
+    agent = create_agent(
+        model=model,
+        system_prompt="You are a precise software designing agent, that designs software based on SOLID design principles." + 
+        "Highlight key components and relationships between components"
+        "You are explicited expected to refrain from writing anything other than Components, relationships, and SOLID principles that are used."
+    )
+    # print("here")
+    response = agent.invoke({"messages": [HumanMessage("Chess")]})
+    print(response["messages"][-1].content)
+ 
