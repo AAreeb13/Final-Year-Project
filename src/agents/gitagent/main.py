74a07b31393f8395cf7ab0e6237ab6ed6e12dc51
Areeb <aaa1322@ic.ac.uk>
2026-05-10 20:03:12 +0100

    feat: agents modules fulder

---
+++ src/agents/gitagent/main.py
@@ -0,0 +1,9 @@
+"""Git Agent main module."""
+
+from __future__ import annotations
+
+
+def build_git_agent() -> None:
+    agent = None
+    raise NotImplementedError("GitAgent setup will be added later.")
+    return agent
